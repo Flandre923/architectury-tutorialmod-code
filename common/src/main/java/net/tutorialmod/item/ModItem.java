3cd79f9d1d61542df67dd0dc9473d6c45ba10cfd
Flandre923 <804984970@qq.com>
2023-10-28 21:52:34 +0800

    添加高级物品

--- common/src/main/java/net/tutorialmod/item/ModItem.java
+++ common/src/main/java/net/tutorialmod/item/ModItem.java
@@ -3,16 +3,21 @@ package net.tutorialmod.item;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.EggItem;
 import net.minecraft.world.item.Item;
 import net.tutorialmod.TutorialMod;
+import net.tutorialmod.item.custom.EightBallItem;
 
 public class ModItem {
 
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(TutorialMod.MOD_ID, Registries.ITEM);
+    // 皓石
     public static final RegistrySupplier<Item> EXAMPLE_ITEM = ITEMS.register("zircon", () ->
             new Item(new Item.Properties().arch$tab(ModCreativeTab.EXAMPLE_TAB)));
-
+    // 高级物品
+    public static final RegistrySupplier<Item> EIGHT_BALL_ITEM = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().arch$tab(ModCreativeTab.EXAMPLE_TAB).stacksTo(1)));
 
     public static void register(){
         ITEMS.register();
